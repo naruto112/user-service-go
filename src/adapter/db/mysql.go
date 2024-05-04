4523ad57bd388970a400adade511499b73054d13
Renato Souza <renatorock3@hotmail.com>
2024-05-03 23:34:34 -0300

    env url de DB

--- src/adapter/db/mysql.go
+++ src/adapter/db/mysql.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	host     = os.Getenv("MYSQL_PRIVATE_URL")
+	host     = os.Getenv("MYSQL_URL")
 	database = os.Getenv("MYSQL_DATABASE")
 	user     = os.Getenv("MYSQLUSER")
 	password = os.Getenv("MYSQLPASSWORD")
